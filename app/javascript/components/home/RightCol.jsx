5bad518de8f43273a61ae1e07b1f9886c9770096
Colin Tück <colin@knowledgeinnovation.eu>
2025-04-27 22:59:09 +0200

    add notice to main page

--- app/javascript/components/home/RightCol.jsx
+++ app/javascript/components/home/RightCol.jsx
@@ -43,24 +43,6 @@ const RightSideHome = () => (
     </p>
 
     <p>
-      <a
-        href="https://github.com/t3-innovation-network/desm/blob/main/README.md"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Instructions for setting up an instance
-      </a>{' '}
-      of the DESM tool are available from the repository.
-    </p>
-
-    <p>
-      <a href="https://github.com/t3-innovation-network/desm/wiki" target="_blank" rel="noreferrer">
-        Instructions for carrying out a mapping
-      </a>{' '}
-      are available on the DESM repository wiki.
-    </p>
-
-    <p>
       DESM is sponsored by the U.S. Chamber of Commerce Foundations&apos; T3 Innovation Network
       program. The T3 Open Competency Network, in partnership with the T3 Data and Technology
       Standards Network is responsible for the requirements and project plans that are used for
@@ -68,9 +50,19 @@ const RightSideHome = () => (
     </p>
 
     <p>
-      If you are interested in participating with a T3 mapping project, please contact Taylor Hanson{' '}
-      <a href="mailto:thansen@uschamber.com">thansen@uschamber.com</a>.
+      This DESM instance has been deployed by the <a href="https://knowledgeinnovation.eu/" target="_blank">
+      Knowledge Innovation Centre (KIC)</a> as part of the <a href="https://quality-link.eu/" target="_blank">
+      QualityLink project</a>.
     </p>
+
+    <div style={{ display: 'flex' }}>
+      <img src="http://quality-link.eu/wp-content/uploads/sites/90/2025/01/EN_Co-fundedbytheEU_RGB_POS.png" style={{ 'margin-right': '15px', width: '200px', 'object-fit': 'contain' }} />
+      <p style={{ margin: '0' }}>
+        The QualityLink project has been funded with support from the European Commission. This website
+        reflects the views only of the authors, and the Commission or the National Agency cannot be held#
+        responsible for any use which may be made of the information contained therein.
+      </p>
+    </div>
   </div>
 );
 
